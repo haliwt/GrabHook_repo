d9da1b7f88745bc3ea15d21a80e95e33d4f2091a
haliwt <haliht@qq.com>
2020-06-13 08:38:35 +0800

    This add information team codes for freeRTOS ,DATA-2020-06-013

--- src/main.c
+++ src/main.c
@@ -32,27 +32,31 @@ SOFTWARE.
 #include "stm32f10x.h"
 
 
+/*
+*************************************************************************
+*                             包含的头文件
+*************************************************************************
+*/
 /* FreeRTOS头文件 */
 #include "FreeRTOS.h"
-
 #include "task.h"
-
+#include "queue.h"
 /* 开发板硬件bsp头文件 */
 #include "bsp_led.h"
 #include "bsp_usart.h"
+#include "bsp_key.h"
 
-#define SOFT_DELAY Delay(0x0FFFFF);
+ /*
+ *************************************************************************
+ *                             函数声明
+ *************************************************************************
+ */
+ static void AppTaskCreate(void);/* 用于创建任务 */
 
-void Delay(__IO u32 nCount);
+ static void LED_Task(void* pvParameters);/* LED_Task任务实现 */
+ static void KEY_Task(void* pvParameters);/* KEY_Task任务实现 */
 
-
-/* Private macro */
-/* Private variables */
- USART_InitTypeDef USART_InitStructure;
-
-/* Private function prototypes */
-/* Private functions */
-
+ static void BSP_Init(void);/* 用于初始化板载相关资源 */
 /**
 **===========================================================================
 **
@@ -62,9 +66,139 @@ void Delay(__IO u32 nCount);
 */
 int main(void)
 {
+	BaseType_t xReturn = pdPASS;/* 定义一个创建信息返回值，默认为pdPASS */
 
+	  /* 开发板硬件初始化 */
+	  BSP_Init();
+
+	  printf("这是一个[野火]-STM32全系列开发板-FreeRTOS任务管理实验！\n\n");
+	  printf("按下KEY1挂起任务，按下KEY2恢复任务\n");
+
+	   /* 创建AppTaskCreate任务 */
+	  xReturn = xTaskCreate((TaskFunction_t )AppTaskCreate,  /* 任务入口函数 */
+	                        (const char*    )"AppTaskCreate",/* 任务名字 */
+	                        (uint16_t       )512,  /* 任务栈大小 */
+	                        (void*          )NULL,/* 任务入口函数参数 */
+	                        (UBaseType_t    )1, /* 任务的优先级 */
+	                        (TaskHandle_t*  )&AppTaskCreate_Handle);/* 任务控制块指针 */
+	  /* 启动任务调度 */
+	  if(pdPASS == xReturn)
+	    vTaskStartScheduler();   /* 启动任务，开启调度 */
+	  else
+	    return -1;
+
+	  while(1);   /* 正常不会执行到这里 */
 
 }
 
+/***********************************************************************
+  * @ 函数名  ： AppTaskCreate
+  * @ 功能说明： 为了方便管理，所有的任务创建函数都放在这个函数里面
+  * @ 参数    ： 无
+  * @ 返回值  ： 无
+  **********************************************************************/
+static void AppTaskCreate(void)
+{
+  BaseType_t xReturn = pdPASS;/* 定义一个创建信息返回值，默认为pdPASS */
+
+  taskENTER_CRITICAL();           //进入临界区
+
+  /* 创建LED_Task任务 */
+  xReturn = xTaskCreate((TaskFunction_t )LED_Task, /* 任务入口函数 */
+                        (const char*    )"LED_Task",/* 任务名字 */
+                        (uint16_t       )512,   /* 任务栈大小 */
+                        (void*          )NULL,	/* 任务入口函数参数 */
+                        (UBaseType_t    )2,	    /* 任务的优先级 */
+                        (TaskHandle_t*  )&LED_Task_Handle);/* 任务控制块指针 */
+  if(pdPASS == xReturn)
+    printf("创建LED_Task任务成功!\r\n");
+  /* 创建KEY_Task任务 */
+  xReturn = xTaskCreate((TaskFunction_t )KEY_Task,  /* 任务入口函数 */
+                        (const char*    )"KEY_Task",/* 任务名字 */
+                        (uint16_t       )512,  /* 任务栈大小 */
+                        (void*          )NULL,/* 任务入口函数参数 */
+                        (UBaseType_t    )3, /* 任务的优先级 */
+                        (TaskHandle_t*  )&KEY_Task_Handle);/* 任务控制块指针 */
+  if(pdPASS == xReturn)
+    printf("创建KEY_Task任务成功!\r\n");
+
+  vTaskDelete(AppTaskCreate_Handle); //删除AppTaskCreate任务
+
+  taskEXIT_CRITICAL();            //退出临界区
+}
+
+
+
+/**********************************************************************
+  * @ 函数名  ： LED_Task
+  * @ 功能说明： LED_Task任务主体
+  * @ 参数    ：
+  * @ 返回值  ： 无
+  ********************************************************************/
+static void LED_Task(void* parameter)
+{
+  while (1)
+  {
+    LED1_ON;
+    printf("LED_Task Running,LED1_ON\r\n");
+    vTaskDelay(500);   /* 延时500个tick */
+
+    LED1_OFF;
+    printf("LED_Task Running,LED1_OFF\r\n");
+    vTaskDelay(500);   /* 延时500个tick */
+  }
+}
+
+/**********************************************************************
+  * @ 函数名  ： LED_Task
+  * @ 功能说明： LED_Task任务主体
+  * @ 参数    ：
+  * @ 返回值  ： 无
+  ********************************************************************/
+static void KEY_Task(void* parameter)
+{
+  while (1)
+  {
+    if( Key_Scan(KEY1_GPIO_PORT,KEY1_GPIO_PIN) == KEY_ON )
+    {/* K1 被按下 */
+      printf("挂起LED任务！\n");
+      vTaskSuspend(LED_Task_Handle);/* 挂起LED任务 */
+      printf("挂起LED任务成功！\n");
+    }
+    if( Key_Scan(KEY2_GPIO_PORT,KEY2_GPIO_PIN) == KEY_ON )
+    {/* K2 被按下 */
+      printf("恢复LED任务！\n");
+      vTaskResume(LED_Task_Handle);/* 恢复LED任务！ */
+      printf("恢复LED任务成功！\n");
+    }
+    vTaskDelay(20);/* 延时20个tick */
+  }
+}
+
+/***********************************************************************
+  * @ 函数名  ： BSP_Init
+  * @ 功能说明： 板级外设初始化，所有板子上的初始化均可放在这个函数里面
+  * @ 参数    ：
+  * @ 返回值  ： 无
+  *********************************************************************/
+static void BSP_Init(void)
+{
+	/*
+	 * STM32中断优先级分组为4，即4bit都用来表示抢占优先级，范围为：0~15
+	 * 优先级分组只需要分组一次即可，以后如果有其他的任务需要用到中断，
+	 * 都统一用这个优先级分组，千万不要再分组，切忌。
+	 */
+	NVIC_PriorityGroupConfig( NVIC_PriorityGroup_4 );
+
+	/* LED 初始化 */
+	LED_GPIO_Config();
+
+	/* 串口初始化	*/
+	USART_Config();
+
+  /* 按键初始化	*/
+  Key_GPIO_Config();
+
+}
 
 
